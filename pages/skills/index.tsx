0900cc6ed2b6d8879ea8f062aab3505afdaf1c0e
Zefri Fahlevi <zefrifahlevi@gmail.com>
2023-04-29 10:26:34 +0700

    New

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Skills() {
   return (
     <>
-      <header classname="text-center font-extrabold">Skills</header>
-      <h1 classname="text-center text-cyan-200">Typescript</h1>
-      <h1 classname="text-center text-purple-200">Javascript</h1>
-      <h1 classname="text-center text-cyan-200">React JS</h1>
-      <h1 classname="text-center text-cyan-200">Next JS</h1>
+      <header className="text-center font-extrabold">Skills</header>
+      <h1 className="text-center text-cyan-200">Typescript</h1>
+      <h1 className="text-center text-purple-200">Javascript</h1>
+      <h1 className="text-center text-cyan-200">React JS</h1>
+      <h1 className="text-center text-cyan-200">Next JS</h1>
     </>
   )
 }
