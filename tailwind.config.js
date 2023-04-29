fd4650c45678f2c85d2e8b6b5b9875cb9fd7dad7
Zefri Fahlevi <zefrifahlevi@gmail.com>
2023-04-29 11:43:36 +0700

    New

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['"Poppins"', "sans-serif"]
+        poppins: ['"Poppins"', "sans-serif"],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        text: "#393F48",
+        brand: "#EF5DA8",
       },
     },
   },
